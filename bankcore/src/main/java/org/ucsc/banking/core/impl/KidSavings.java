b149f3fe8344ee64c84c5a545977c3fcf5788bb6
muthulee <dimuthul@wso2.com>
2016-06-14 11:15:28 +0530

    Changing...

--- bankcore/src/main/java/org/ucsc/banking/core/impl/KidSavings.java
+++ bankcore/src/main/java/org/ucsc/banking/core/impl/KidSavings.java
@@ -2,11 +2,10 @@ package org.ucsc.banking.core.impl;
 
 import org.ucsc.banking.core.model.AccountType;
 
-//public class KidSavings implements AccountType {
-public class KidSavings {
+public class KidSavings implements AccountType {
 
 	public int getAccountType() {
-	    return 0;
+	    return 1;
     }
 
 	public String getAccountTypeName() {
