0a7fd8c3c6e9ed7aed4903fb3dacfe6719d36d8f
dimuthu <dimuthul@wso2.com>
2016-06-14 11:05:57 +0530

    Adding hte files.

---
+++ bankcore/src/main/java/org/ucsc/banking/core/impl/KidSavings.java
@@ -0,0 +1,18 @@
+package org.ucsc.banking.core.impl;
+
+import org.ucsc.banking.core.model.AccountType;
+
+//public class KidSavings implements AccountType {
+public class KidSavings {
+
+	public int getAccountType() {
+	    return 0;
+    }
+
+	public String getAccountTypeName() {
+	    return null;
+    }
+	
+	
+
+}
