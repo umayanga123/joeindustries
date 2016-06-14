b3afab2901fd5b2f3a4ccab006cdbdf0d33c7b0f
muthulee <dimuthul@wso2.com>
2016-06-14 11:21:54 +0530

    Update KidSavings.java

--- bankcore/src/main/java/org/ucsc/banking/core/impl/KidSavings.java
+++ bankcore/src/main/java/org/ucsc/banking/core/impl/KidSavings.java
@@ -5,7 +5,7 @@ import org.ucsc.banking.core.model.AccountType;
 public class KidSavings implements AccountType {
 
 	public int getAccountType() {
-	    return 1
+	    return 1;
     }
 
 	public String getAccountTypeName() {
